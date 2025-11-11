3a7152e8aa0f6349e43312fb19144728e15f2a7c
Nikita Karnygin <nikita.karnygin2505@gmail.com>
2025-11-12 00:15:51 +0200

    Initial Commit

---
+++ AdBlocker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AdBlocker
+//
+//  Created by Nikita on 12.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
