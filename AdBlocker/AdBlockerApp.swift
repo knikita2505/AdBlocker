f0db00ab8229abe5ac7b34cc18c72a288bf26853
Nikita Karnygin <nikita.karnygin2505@gmail.com>
2025-11-13 20:05:50 +0200

    init

---
+++ AdBlocker/AdBlockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdBlockerApp.swift
+//  AdBlocker
+//
+//  Created by Nikita on 12.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct AdBlockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
